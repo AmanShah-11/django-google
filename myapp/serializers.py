da9f2a1486e790a03fd545d97108193a42134a79
Aman Shah <a232shah@uwaterloo.ca>
2021-01-05 21:33:05 -0500

    Added new completed field through serializer

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Schedule
+import datetime
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    completed = serializers.SerializerMethodField()
+    def get_completed(self, obj):
+        time_difference = datetime.date.today() - obj.date
+        if (time_difference.total_Seconds() >= 0):
+            return True
+        return False
+
     class Meta:
         model = Schedule
-        fields = ("id", "time_start", "time_end", "date", "activity", "user_invite")
+        fields = ("id", "time_start", "time_end", "date", "activity", "user_invite", "completed")
