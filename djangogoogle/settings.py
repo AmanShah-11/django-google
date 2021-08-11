ecd1bc39505dad3a5af5f4f07b9bfa15c80c2a4e
Aman Shah <a232shah@uwaterloo.ca>
2021-08-11 09:07:41 -0400

    temp

--- djangogoogle/settings.py
+++ djangogoogle/settings.py
@@ -61,8 +61,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
